b121ba5f160ce242b0f87e3c1a82678f801c056c
Joao Ureta <joao.ureta@gmail.com>
2024-05-30 22:51:44 -0400

    version 2.2

--- Epe2_Nicken_Joao/WebForm5.aspx.cs
+++ Epe2_Nicken_Joao/WebForm5.aspx.cs
@@ -18,7 +18,41 @@ namespace Epe2_Nicken_Joao
 
         protected void btnCalcular_Click(object sender, EventArgs e)
         {
-           
+            try
+            {
+                int[] numeros = new int[100];
+
+                numeros[0] = Convert.ToDouble(txtnumeros.text);
+                numeros[1] = Convert.ToDouble(txtnumeros.text);
+                numeros[2] = Convert.ToDouble(txtnumeros.text);
+                numeros[3] = Convert.ToDouble(txtnumeros.text);
+                numeros[4] = Convert.ToDouble(txtnumeros.text);
+                numeros[5] = Convert.ToDouble(txtnumeros.text);
+                numeros[6] = Convert.ToDouble(txtnumeros.text);
+                numeros[7] = Convert.ToDouble(txtnumeros.text);
+                numeros[8] = Convert.ToDouble(txtnumeros.text);
+                numeros[9] = Convert.ToDouble(txtnumeros.text);
+
+                int suma = 0;
+                double promedio = suma / Convert.ToDouble(txtnumeros.text);
+
+                for(int i=0; i< numeros.Length; i++)
+                {
+                    suma += numeros[i];
+                    promedio = numeros[i];
+                    
+                }
+
+                lblcalcular.text = +suma;
+                lblpromedio.text = +promedio;
+
+            }
+            catch (Exception ex)
+            {
+                // Manejo de errores (por ejemplo, formato incorrecto)
+                lblCalcular.Text = "Error: Verifique los valores ingresados.";
+                lblPromedio.Text = "";
+            }
         }
 
         protected void btnVolver_Click(object sender, EventArgs e)
